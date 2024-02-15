fa1bb01d549862502c6e58a2ca62c8098620371c
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-16 02:16:20 +0400

    initial commit

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+import classes from "./main-navigation.module.css";
+
+function MainNavigation() {
+  const { session, loading } = useSession();
+  console.log("SESSION: ", session, "LOADING: ", loading);
+
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <div className={classes.logo}>Next Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
