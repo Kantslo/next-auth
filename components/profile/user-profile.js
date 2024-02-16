ac17a8a25a235f2613fd979dd4372c26aeaab94f
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-16 15:20:36 +0400

    feat: added redirect function on login and logout using getServerSideProps

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,23 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession, getSession } from "next-auth/react";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     }
+  //   });
+  // }, []);
+  // const { data: session, status } = useSession();
+
+  // if (status === "loading") {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
